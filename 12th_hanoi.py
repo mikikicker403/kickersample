afd640fd0b26f13b010091e10d5b05a93d1947b1
kick <mikikicker@gmail.com>
2019-10-20 14:50:47 +0900

    2019-10-20(14:50:47)

---
+++ 12th_hanoi.py
@@ -0,0 +1,12 @@
+
+#-*- coding:utf-8 -*-
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('program start')
+#logging.disable(logging.CRITICAL)
+
+if __name__=='__main__':
+    pass
+
+
+    

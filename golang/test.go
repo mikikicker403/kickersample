b626b46df166b423bb796dbdf6a086492ab7d5b9
kick <mikikicker@gmail.com>
2021-03-14 18:32:27 +0900

    20210314

--- golang/test.go
+++ golang/test.go
@@ -7,6 +7,20 @@ import (
         )
 func main(){
     fmt.Println("template")
+    var n int=100
+    var m int
+    m=100
+
+    var l=100
+    //関数内
+    //var a :=100
+
+    /*var (
+        b=100
+        c=200
+        )
+    */
+
 
 }//endfunc(main)
     
